6f57d6f961ca3b5cf24d3ac16b3e6adfb2be99a6
Bhalchandra Gajare <gajare@codeaurora.org>
2014-03-27 18:59:33 -0700

    ASoC: wcd_cpe_core: Add core support for CPE
    CPE (Codec Processing Engine) core support enables the control of the
    CPE hardware block from codec driver and also enables the alsa platform
    / cpu dai, etc drivers to interact with the CPE hardware block on the
    codec. Add support for CPE core.
    
    Change-Id: I5be40e880af81d9e4a663075f77022d6a0e8b611
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>
    
    Conflicts:
    	include/sound/cpe_core.h
    	sound/soc/codecs/Makefile
    	sound/soc/codecs/wcd_cpe_core.c
    	sound/soc/codecs/wcd_cpe_core.h

--- sound/soc/codecs/wcd_cpe_core.h
+++ sound/soc/codecs/wcd_cpe_core.h
@@ -16,6 +16,30 @@
 
 #define WCD_CPE_LAB_MAX_LATENCY 250
 #define WCD_CPE_MAD_SLIM_CHANNEL 140
+
+/* Indicates CPE block is ready for image re-download */
+#define WCD_CPE_BLK_READY  (1 << 0)
+/* Indicates the underlying bus is ready */
+#define WCD_CPE_BUS_READY (1 << 1)
+
+/*
+ * only when the underlying bus and CPE block both are ready,
+ * the state will be ready to download
+ */
+#define WCD_CPE_READY_TO_DLOAD	\
+	(WCD_CPE_BLK_READY | WCD_CPE_BUS_READY)
+
+#define WCD_CPE_LOAD_IMEM (1 << 0)
+#define WCD_CPE_LOAD_DATA (1 << 1)
+#define WCD_CPE_LOAD_ALL \
+	(WCD_CPE_LOAD_IMEM | WCD_CPE_LOAD_DATA)
+
+enum {
+	WCD_CPE_LSM_CAL_AFE = 0,
+	WCD_CPE_LSM_CAL_LSM,
+	WCD_CPE_LSM_CAL_MAX,
+};
+
 struct wcd_cpe_cdc_cb {
 	/* codec provided callback to enable RCO */
 	int (*cdc_clk_en) (struct snd_soc_codec *, bool);
@@ -23,7 +47,31 @@ struct wcd_cpe_cdc_cb {
 	/* callback for FLL setup for codec */
 	int (*cpe_clk_en) (struct snd_soc_codec *, bool);
 	int (*cdc_ext_clk)(struct snd_soc_codec *codec, int enable, bool dapm);
-	int (*slimtx_lab_en)(struct snd_soc_codec *codec, int event);
+	int (*lab_cdc_ch_ctl)(struct snd_soc_codec *codec, u8 event);
+};
+
+enum wcd_cpe_ssr_state_event {
+	/* Indicates CPE is initialized */
+	WCD_CPE_INITIALIZED = 0,
+	/* Indicates that IMEM is downloaded to CPE */
+	WCD_CPE_IMEM_DOWNLOADED,
+	/* Indicates CPE is enabled */
+	WCD_CPE_ENABLED,
+	/* Indicates that CPE is currently active */
+	WCD_CPE_ACTIVE,
+	/* Event from underlying bus notifying bus is down */
+	WCD_CPE_BUS_DOWN_EVENT,
+	/* Event from CPE block, notifying CPE is down */
+	WCD_CPE_SSR_EVENT,
+	/* Event from underlying bus notifying bus is up */
+	WCD_CPE_BUS_UP_EVENT,
+};
+
+struct wcd_cpe_ssr_entry {
+	int offline;
+	u32 offline_change;
+	wait_queue_head_t offline_poll_wait;
+	struct snd_info_entry *entry;
 };
 
 struct wcd_cpe_core {
@@ -104,12 +152,14 @@ struct wcd_cpe_params {
 	struct snd_soc_codec *codec;
 	struct wcd_cpe_core * (*get_cpe_core) (
 				struct snd_soc_codec *);
-	struct wcd_cpe_cdc_cb *cdc_cb;
+	const struct wcd_cpe_cdc_cb *cdc_cb;
 	int dbg_mode;
 	u16 cdc_major_ver;
 	u16 cdc_minor_ver;
 	u32 cdc_id;
 };
 
-struct wcd_cpe_core *wcd_cpe_init_and_boot(const char *,
-	struct snd_soc_codec *, struct wcd_cpe_params *params);
+int wcd_cpe_ssr_event(void *core_handle,
+		      enum wcd_cpe_ssr_state_event event);
+struct wcd_cpe_core *wcd_cpe_init(const char *,
+struct snd_soc_codec *, struct wcd_cpe_params *params);
