0b305016cd7e13f9860a735d41ef73e51b3784ec
baran0119 <baran0119@gmail.com>
2015-07-26 08:06:59 +0200

    qdsp6v2: Try upgrade [4/x]

--- drivers/staging/android/uapi/android_alarm.h
+++ drivers/staging/android/uapi/android_alarm.h
@@ -1,4 +1,4 @@
-/* include/linux/android_alarm.h
+/* drivers/staging/android/uapi/android_alarm.h
  *
  * Copyright (C) 2006-2007 Google, Inc.
  *
@@ -13,12 +13,11 @@
  *
  */
 
-#ifndef _LINUX_ANDROID_ALARM_H
-#define _LINUX_ANDROID_ALARM_H
+#ifndef _UAPI_LINUX_ANDROID_ALARM_H
+#define _UAPI_LINUX_ANDROID_ALARM_H
 
 #include <linux/ioctl.h>
 #include <linux/time.h>
-#include <linux/compat.h>
 
 enum android_alarm_type {
 	/* return code bit numbers or set alarm arg */
@@ -60,28 +59,7 @@ enum android_alarm_return_flags {
 #define ANDROID_ALARM_SET_AND_WAIT(type)    ALARM_IOW(3, type, struct timespec)
 #define ANDROID_ALARM_GET_TIME(type)        ALARM_IOW(4, type, struct timespec)
 #define ANDROID_ALARM_SET_RTC               _IOW('a', 5, struct timespec)
-#if defined(CONFIG_RTC_AUTO_PWRON)
-#define ANDROID_ALARM_SET_ALARM_BOOT	    _IOW('a', 7, struct timespec)
-#endif
 #define ANDROID_ALARM_BASE_CMD(cmd)         (cmd & ~(_IOC(0, 0, 0xf0, 0)))
 #define ANDROID_ALARM_IOCTL_TO_TYPE(cmd)    (_IOC_NR(cmd) >> 4)
-
-
-#ifdef CONFIG_COMPAT
-#define ANDROID_ALARM_SET_COMPAT(type)		ALARM_IOW(2, type, \
-							struct compat_timespec)
-#define ANDROID_ALARM_SET_AND_WAIT_COMPAT(type)	ALARM_IOW(3, type, \
-							struct compat_timespec)
-#define ANDROID_ALARM_GET_TIME_COMPAT(type)	ALARM_IOW(4, type, \
-							struct compat_timespec)
-#define ANDROID_ALARM_SET_RTC_COMPAT		_IOW('a', 5, \
-							struct compat_timespec)
-#define ANDROID_ALARM_IOCTL_NR(cmd)		(_IOC_NR(cmd) & ((1<<4)-1))
-#define ANDROID_ALARM_COMPAT_TO_NORM(cmd)  \
-				ALARM_IOW(ANDROID_ALARM_IOCTL_NR(cmd), \
-					ANDROID_ALARM_IOCTL_TO_TYPE(cmd), \
-					struct timespec)
-
-#endif
 
 #endif
