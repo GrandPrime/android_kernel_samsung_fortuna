ddde33036ad5c25035df2e72f186aaa71ac18b89
baran0119 <baran0119@gmail.com>
2015-07-29 16:42:47 +0200

    build_kernel.sh: Add some stuff

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,12 @@
+START_TIME=$SECONDS
+
+rm -fr out
 export ARCH=arm
 mkdir $(pwd)/out
 make -j$(cat /proc/cpuinfo | grep processor | wc -l) -C $(pwd) O=$(pwd)/out msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_aio_defconfig DEBUG_DEFCONFIG= SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG= TIMA_DEFCONFIG= 
 make -j$(cat /proc/cpuinfo | grep processor | wc -l) -C $(pwd) O=$(pwd)/out
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
+echo -e "\t$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
