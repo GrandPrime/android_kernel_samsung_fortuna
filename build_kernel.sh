a42bef35c2ba51e6e79dd78eb7eb7fd068ac45f3
baran0119 <baran0119@gmail.com>
2015-07-31 19:01:23 +0200

    build_kernel.sh: my update

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,8 +3,17 @@ START_TIME=$SECONDS
 rm -fr out
 export ARCH=arm
 mkdir $(pwd)/out
-make -j$(cat /proc/cpuinfo | grep processor | wc -l) -C $(pwd) O=$(pwd)/out msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_aio_defconfig DEBUG_DEFCONFIG= SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG= TIMA_DEFCONFIG= 
+
+echo Preparing
+make -j$(cat /proc/cpuinfo | grep processor | wc -l) -C $(pwd) O=$(pwd)/out msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_aio_defconfig DEBUG_DEFCONFIG= SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG= TIMA_DEFCONFIG=
+echo Building
 make -j$(cat /proc/cpuinfo | grep processor | wc -l) -C $(pwd) O=$(pwd)/out
+
+# echo Preparing
+# make -j1 -C $(pwd) O=$(pwd)/out msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_aio_defconfig DEBUG_DEFCONFIG= SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG= TIMA_DEFCONFIG= 
+# echo Building
+# make -j1 -C $(pwd) O=$(pwd)/out
+
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
